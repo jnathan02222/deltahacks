3bb7ec7d241cfe27f8e67b3079578f4dc5ba08c6
jnathan0222 <123518866+jnathan0222@users.noreply.github.com>
2025-01-11 20:21:18 -0500

    facial detection, more like racial detection

--- file_interface/app/game/page.js
+++ file_interface/app/game/page.js
@@ -3,12 +3,38 @@ import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { RotateLoader } from "react-spinners";
+import {FaceDetector, FilesetResolver, Detection} from '@mediapipe/tasks-vision'
+
 export default function Home() {
     const [gameState, setGameState] = useState("pregame");
     const [question, setQuestion] = useState("");
     const ws = useRef();
+    const videoElem = useRef();
+    const faceDetector  = useRef();
 
     useEffect(()=>{ 
+                
+        // Initialize the object detector
+        const initializefaceDetector = async () => {
+          const vision = await FilesetResolver.forVisionTasks(
+            "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+          );
+          faceDetector.current = await FaceDetector.createFromOptions(vision, {
+            baseOptions: {
+              modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite`,
+              delegate: "GPU"
+            },
+            runningMode: "VIDEO"
+          });
+        };
+        initializefaceDetector();
+        
+
+
+
+
+
+        //Websocket setup
         ws.current = new WebSocket('ws://localhost:8081/ws');
         ws.current.addEventListener("open", (event) => {
           console.log("Connected to server.");
@@ -24,9 +50,7 @@ export default function Home() {
         
         
 
-        axios.get('/question').then(
-            response => setQuestion(response.data["q"])
-        )
+        
 
         return () => {
           if (ws.current) {
@@ -43,6 +67,29 @@ export default function Home() {
         speechSynthesis.speak(utterance);
     }, [question])
 
+    useEffect(()=>{
+        if(gameState === "choosing"){
+            
+        }
+    }, [gameState])
+
+    let lastVideoTime = useRef(-1);
+    async function predictWebcam() {
+        // if image mode is initialized, create a new classifier with video runningMode
+        let video = videoElem.current;
+        let startTimeMs = performance.now();
+    
+        // Detect faces using detectForVideo
+        if (video.currentTime !== lastVideoTime.current) {
+            lastVideoTime.current = video.currentTime;
+            const detections = faceDetector.current.detectForVideo(video, startTimeMs).detections;
+            console.log(detections)
+
+        }
+        // Call this function again to keep predicting when the browser is ready
+        window.requestAnimationFrame(predictWebcam);
+    }
+
     return (
         <main className="flex flex-col  max-h-screen justify-center	items-center p-24">
             <div className="flex items-center">
@@ -56,6 +103,18 @@ export default function Home() {
                     audio.loop = true;
                     audio.play();
                     setGameState("choosing")
+
+                    navigator.mediaDevices.getUserMedia({video: true}).then(
+                        (stream) => {
+                            const video = videoElem.current;
+                            if(!video){
+                                return;
+                            }
+                            video.srcObject = stream;
+                            video.addEventListener("loadeddata", predictWebcam);
+
+                        }
+                    )
                 }} className="p-2 m-2 border-black rounded-md flex items-center mt-10"><Image src="/play.png" width={100} height={100} alt="Bones"></Image></button>
             </div>}
             {gameState === 'choosing' && <div className="flex flex-col items-center italic">
@@ -66,6 +125,18 @@ export default function Home() {
                 <h2 className="text-4xl p-10 w-3/4 text-center text-gray-600">{question}</h2>
                 <h2 className="text-4xl p-5 w-3/4 text-center text-gray-400">Say your answer:</h2>
             </>}
+            <video ref={videoElem} id="webcam" autoPlay={true} playsInline={true} className="" style={{width: 320, height:240}}></video>
+
         </main>
     );
 }
+
+/*
+axios.get('/question').then(
+    response => setQuestion(response.data["q"])
+)
+
+Detect faces
+Speech to text
+Check answer (use embeddings?)
+*/
